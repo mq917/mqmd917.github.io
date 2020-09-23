95c71f35eb7175694226c90d592260a8a6e7d7d9
mq917 <61832580+mq917@users.noreply.github.com>
2020-09-23 12:02:47 +0400

    change all

---
+++ js/script.js
@@ -0,0 +1,70 @@
+window.addEventListener('DOMContentLoaded', () => {
+    function init() {
+        // let request = new XMLHttpRequest();
+        // request.open('GET', 'http://localhost:3000/people');
+        // request.setRequestHeader('Content-type', 'application/json; charset = utf-8');
+        // request.send();
+
+        // request.addEventListener('readystatechange', function() {
+        //     if (request.readyState === 4 && request.status == 200) {
+        //         let data = JSON.parse(request.response);
+        //         data.forEach(item => {
+        //             let card = document.createElement('div');
+
+        //             card.classList.add('card');
+
+        //             let icon;
+        //             if (item.sex === "male") {
+        //                 icon = "icons/mars.png";
+        //             } else {
+        //                 icon = "icons/female.png";
+        //             }
+
+        //             card.innerHTML = `
+        //             <img src="${ item.photo }" alt="persone">
+        //             <div class = "name"> ${item.name}</div>
+        //             <div class = " sex">
+        //             <img src= ${icon} alt=${item.sex}>
+        //             </div>
+        //             <div class = "age"> ${item.age}</div>`;
+        //             document.querySelector('.app').appendChild(card);
+
+        //         });
+        //     } else {
+        //         console.error('Chto-to poshlo ne tak!');
+        //     }
+        // });
+        fetch('http://localhost:3000/people')
+            .then(data => data.json())
+            .then(data => createCards(data))
+            .catch(err => console.error(err));
+        this.remove();
+    }
+
+    function createCards(response) {
+        response.forEach(item => {
+            let card = document.createElement('div');
+
+            card.classList.add('card');
+
+            let icon;
+            if (item.sex === "male") {
+                icon = "icons/mars.png";
+            } else {
+                icon = "icons/female.png";
+            }
+
+            card.innerHTML = `
+                        <img src="${ item.photo }" alt="persone">
+                        <div class = "name"> ${item.name}</div>
+                        <div class = " sex">
+                        <img src= ${icon} alt=${item.sex}>
+                        </div>
+                        <div class = "age"> ${item.age}</div>`;
+            document.querySelector('.app').appendChild(card);
+        });
+    }
+
+    document.querySelector('.button').addEventListener('click', init, { "once": true });
+
+});
